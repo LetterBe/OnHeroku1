242348776b17aecaa1843f21e75cdd2208bf24f7
Bárbara <barbarap@tutanota.com>
2022-03-08 11:21:40 +0100

    first commit on heroku

---
+++ src/main/java/de/neuefische/herokudemo/GrettingController.java
@@ -0,0 +1,18 @@
+package de.neuefische.herokudemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/greeting")
+public class GrettingController {
+
+    @GetMapping("/{name}")
+    public String greet(@PathVariable String name) {
+        return "Hallo " +  name;
+    }
+
+}
+
